6ab146849c6c97bbc9bbd91afbc66a73f048fae8
Charith Lakshan <it23841086@my.sliit.lk>
2025-06-24 10:03:16 +0530

    Create add_post.php

---
+++ blog/add_post.php
@@ -0,0 +1,16 @@
+<!-- add_post.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Post</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Add New Post</h1>
+        <nav>
+            <a href="blog.php">Home</a>
+        </nav>
+    </header>
