692e8f3a3398da372f4ff2f4904b16214728f388
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-27 09:11:55 +0530

    Update

--- clinic/create.php
+++ clinic/create.php
@@ -44,3 +44,84 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
             $errorMessage = "Only JPG, JPEG, PNG & GIF files are allowed.";
         }
     }
+    
+
+    do{
+        if(empty($ClinicName)||empty($StaffInCharge)||empty($Address)){
+            $errorMessage="All the fields are required";
+            break;
+        }
+
+        //add new client to database
+        $sql="INSERT INTO clinicdetails(clinicImage,clinicName,staffInCharge,address)".
+                "VALUES('$ClinicImage','$ClinicName','$StaffInCharge','$Address')";
+        $result=$conn->query($sql);
+
+        if(!$result){
+            $errorMessage="Invalid query:".$conn->error;
+            break;
+        }
+
+        $ClinicImage="";
+        $ClinicName="";
+        $StaffInCharge="";
+        $Address="";
+
+        $successMessage="Clinic added correctly";
+        header("location:index.php");
+    exit;
+        
+    }while(false);
+    
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Clinic</title>
+        <link rel="stylesheet" href="../CSS/clinic.css">
+    </head>
+    <body>
+        <h2>New Clinic</h2>
+
+        <form method="POST"enctype="multipart/form-data">
+            <div class="f1">
+                <label>Image:</label>
+                <div>
+                <input type="file" name="clinicImage" required >
+                </div>
+            </div>
+            <div class="f2">
+                <label class="f_l2">Clinic Name</label>
+                <div class="textbox2">
+                    <input type="text" class="f_t2" name="clinicName" value="<?php echo $ClinicName; ?>"required>
+                </div>
+            </div>
+
+            <div class="f3">
+                <label class="f_l3">Doctor-In-Charge</label>
+                <div class="textbox3">
+                    <input type="text" class="f_t3" name="staffInCharge" value="<?php echo $StaffInCharge; ?>"required>
+                </div>
+            </div>
+
+            <div class="f4">
+                <label class="f_l4">Address</label>
+                <div class="textbox4">
+                    <input type="text" class="f_t4" name="address" value="<?php echo $Address; ?>" required>
+                </div>
+            </div>
+            
+            <div class="f5">
+                <div class="f_l5">
+                    <button type="submit" class="btn">Submit</button>
+                </div>
+                
+                <div class="f_l5">
+                    <button><a class="btn" href="index.php" role="button">Cancel</a></button>
+                </div>
+            </div>
+        </form>
+    </body>
+</html>
