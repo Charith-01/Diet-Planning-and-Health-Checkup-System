9f1758360c9a34925f5a65666e528c581fe08c02
Charith Lakshan <it23841086@my.sliit.lk>
2025-08-10 10:48:53 +0530

    Create dindex.php

---
+++ health_record_admin/dindex.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+    <style>
+        h1 {
+                text-align: center;
+                color: #2c6fbb;
+                margin-top: 20px;
+                margin-bottom: 20px;
+            }
+        .question {
+
+        }
+        .constainer{
+                max-width: 600px;
+                margin: 0px auto;
+                padding: 20px;
+                border: 2px solid #ccc;
+                border-radius: 10px;
+                background-color: #f9f9f9;
+            }
+            .buttons input[type="submit"] {
+            background-color: red;
+        }.buttons {
+            text-align: center;
+            }
+            .buttons input {
+            margin: 5px;
+            margin-top:30px;
+            margin-bottom:0px;
+            padding-bottom: 0px;
+            padding: 7px 12px;
+            border: none;
+            color: white;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+    </style>
